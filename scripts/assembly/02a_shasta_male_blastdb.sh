9d2112cf7b09b8e62dd44d52ed311132f092b278
Noah Reid <noah.reid@uconn.edu>
2020-02-10 13:58:59 -0500

    blastdb

---
+++ scripts/assembly/02a_shasta_male_blastdb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+#SBATCH --job-name=makefastas
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=5G
+#SBATCH --qos=general
+#SBATCH --partition=general
+#SBATCH --mail-user=noah.reid@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+
+module load blast/2.7.1
+
+# input/output files, directories
+INDIR=../../results/shasta_male_himem2
+INFILE=../../results/shasta_male_himem2/Assembly.fasta
+
+OUTDIR=../../results/shasta_male_himem2-blastdb
+mkdir -p $OUTDIR
+
+
+makeblastdb -in - \
+-dbtype nucl \
+-title shasta_male.blastdb \
+-parse_seqids \
+-out $OUTDIR/shasta_male.blastdb
+
