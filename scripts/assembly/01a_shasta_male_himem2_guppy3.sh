1db4346e3530a5a9657eff5e87952b06f51d7e6b
Noah Reid <noah.reid@uconn.edu>
2020-02-10 12:43:55 -0500

    shasta

---
+++ scripts/assembly/01a_shasta_male_himem2_guppy3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=shasta_assembly_male
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 36
+#SBATCH --partition=himem2
+#SBATCH --qos=himem
+#SBATCH --mail-type=ALL
+#SBATCH --mem=900G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load shasta/0.4.0
+
+FASTA=../../results/fastas/male.fasta
+
+OUTDIR=../../results/shasta_male_himem2_guppy3
+rm -r $OUTDIR
+
+# shasta requires uncompressed fasta
+shasta \
+--input $FASTA  \
+--assemblyDirectory $OUTDIR \
+--Reads.minReadLength 500 \
+--memoryMode anonymous \
+--memoryBacking 4K \
+--threads 36 \
+--Assembly.consensusCaller guppy-3.0.5-a
