9fbdd42760813726c9504366ad9fe45417a011ac
Noah Reid <noah.reid@uconn.edu>
2020-02-11 14:32:06 -0500

    minimap2

---
+++ scripts/assembly/03a_minimap_male.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=map_reads
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 24
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mem=25G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+
+module load minimap2/2.17
+
+GENOME=../../results/shasta_male_himem2/Assembly.fasta
+FASTQ=../../results/fastqs/male.fastq.gz
+
+minimap2 \
+-ax map-ont \
+-t 24 \
+$GENOME \
+$FASTQ
+
