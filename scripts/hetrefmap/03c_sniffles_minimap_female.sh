fcc0ddd5bc826ceaa04b1af353694c1e0fa3b4d0
Noah Reid <noah.reid@uconn.edu>
2020-12-19 11:17:24 -0500

    sniffles

---
+++ scripts/hetrefmap/03c_sniffles_minimap_female.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=minimap_female
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 15
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mem=25G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+# load software
+module load htslib/1.10.2
+SNIFFLES=~/bin/Sniffles-master/bin/sniffles-core-1.0.12/sniffles
+
+
+# input/output dirs, files
+
+INFILE=/projects/EBP/CBC/Reid_promethion/grandis_ont/results/hetrefmap/female.minimap.bam
+
+OUTDIR=../../results/hetrefmap_SVs/
+mkdir -p $OUTDIR
+
+
+# run sniffles
+$SNIFFLES -m $INFILE -v $OUTDIR/female_minimap.vcf
+
+# compress and index
+bgzip $OUTDIR/female_minimap.vcf
+tabix -p vcf $OUTDIR/female_minimap.vcf.gz
+
