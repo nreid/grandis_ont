24e485e609cc56bfca88b6b3095d6899b022a76f
Noah Reid <noah.reid@uconn.edu>
2020-12-21 13:26:23 -0500

    assembly

--- scripts/assembly/01a_shasta_0.7.0.sh
+++ scripts/assembly/01a_shasta_0.7.0.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=shasta_assembly_male
+#SBATCH --job-name=shasta_v0.7.0_male
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 64
-#SBATCH --partition=himem
+#SBATCH -c 36
+#SBATCH --partition=himem2
 #SBATCH --qos=himem
 #SBATCH --mail-type=ALL
-#SBATCH --mem=500G
+#SBATCH --mem=900G
 #SBATCH --mail-user=noah.reid@uconn.edu
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
 
-module load shasta/0.2.0
+module load shasta/0.7.0
 
 FASTA=../../results/fastas/male.fasta
 
-OUTDIR=../../results/shasta_male
+OUTDIR=../../results/shasta_male_v0.7.0
 rm -r $OUTDIR
 
 # shasta requires uncompressed fasta
@@ -25,4 +25,5 @@ shasta \
 --Reads.minReadLength 500 \
 --memoryMode anonymous \
 --memoryBacking 4K \
---threads 64
+--threads 36 \
+--Assembly.detangleMethod 1
