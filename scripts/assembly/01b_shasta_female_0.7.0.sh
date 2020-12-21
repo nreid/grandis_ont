3a58cab357ba3f6e2550d5132b8e9aad06719135
Noah Reid <noah.reid@uconn.edu>
2020-12-21 13:29:54 -0500

    assemble male

---
+++ scripts/assembly/01b_shasta_female_0.7.0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=shasta_v0.7.0_female
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 36
+#SBATCH --partition=himem2
+#SBATCH --qos=himem
+#SBATCH --mail-type=ALL
+#SBATCH --mem=900G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load shasta/0.7.0
+
+FASTA=../../results/fastas/female.fasta
+
+OUTDIR=../../results/shasta_female_v0.7.0
+rm -r $OUTDIR
+
+# shasta requires uncompressed fasta
+shasta \
+--input $FASTA  \
+--assemblyDirectory $OUTDIR \
+--Reads.minReadLength 500 \
+--memoryMode anonymous \
+--memoryBacking 4K \
+--threads 36 \
+--Assembly.detangleMethod 1
