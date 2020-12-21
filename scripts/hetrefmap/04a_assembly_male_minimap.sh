6564e13eeeab922778b7102d12b5e90ff5beba2e
Noah Reid <noah.reid@uconn.edu>
2020-12-20 20:09:03 -0500

    assembly map

---
+++ scripts/hetrefmap/04a_assembly_male_minimap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=minimap_assembly_male
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 15
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mem=25G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+
+# load software
+module load minimap2/2.17
+module load samtools/1.9
+
+# input/output dirs, files
+
+# this assembly has decent N50
+INDIR=/projects/EBP/CBC/Reid_promethion/grandis_ont/results/shasta_male_himem2
+
+OUTDIR=../../results/hetrefmap/
+mkdir -p $OUTDIR
+
+GENOME=../../genome/GCF_011125445.2_MU-UCD_Fhet_4.1_genomic.fna
+
+# run minimap2
+minimap2 -c --MD -ax map-ont -t 10 $GENOME $INDIR/Assembly.fasta | \
+samtools sort -@ 5 -T $OUTDIR/male.assembly.minimap -O BAM \
+>$OUTDIR/male.minimap.bam
+
+samtools index $OUTDIR/male.minimap.bam
