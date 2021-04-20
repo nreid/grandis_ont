dd347e9957617fe35c0dccf8698abbf793515de4
Noah Reid <noah.reid@uconn.edu>
2021-04-20 12:28:54 -0400

    combined

---
+++ scripts/assembly/01c_shasta_combined_0.7.0.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=shasta_v0.7.0_combined
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 36
+#SBATCH --partition=himem2
+#SBATCH --qos=himem
+#SBATCH --mail-type=ALL
+#SBATCH --mem=900G
+#SBATCH --mail-user=noah.reid@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+module load shasta/0.7.0
+
+FASTA=../../results/fastas/combined.fasta
+
+OUTDIR=../../results/shasta_combined_v0.7.0
+rm -r $OUTDIR
+
+# shasta requires uncompressed fasta
+shasta \
+--input $FASTA  \
+--assemblyDirectory $OUTDIR \
+--Reads.minReadLength 500 \
+--memoryMode anonymous \
+--memoryBacking 4K \
+--threads 36 \
+--Assembly.detangleMethod 1
