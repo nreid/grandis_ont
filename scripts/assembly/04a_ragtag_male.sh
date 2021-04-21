9a026c410cae69a3d076c653856ca23f5b881ee3
Noah Reid <noah.reid@uconn.edu>
2021-04-21 14:15:34 -0400

    ragtag

--- scripts/assembly/04a_ragtag_male.sh
+++ scripts/assembly/04a_ragtag_male.sh
@@ -29,7 +29,7 @@ QUERY=../../results/shasta_male_v0.7.0/medaka_consensus/consensus.fasta
 REFERENCE=../../genome/GCF_011125445.2_MU-UCD_Fhet_4.1_genomic.fna
 
 # correct
-ragtag.py correct -t 12 --inter $REFERENCE $QUERY
+ragtag.py correct -o $OUTDIR -u -t 12 --inter $REFERENCE $QUERY
 
 # scaffold
-ragtag.py scaffold -t 12 $REFERENCE $OUTDIR/query.corrected.fasta
+ragtag.py scaffold -o $OUTDIR -u -t 12 $REFERENCE $OUTDIR/consensus.corrected.fasta
