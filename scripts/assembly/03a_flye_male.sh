d53f457520cc5c92f255f9df10477079d969c809
Noah Reid <noah.reid@uconn.edu>
2021-04-22 14:15:06 -0400

    flye

--- scripts/assembly/03a_flye_male.sh
+++ scripts/assembly/03a_flye_male.sh
@@ -3,10 +3,10 @@
 #SBATCH -N 1
 #SBATCH -n 1
 #SBATCH -c 24
-#SBATCH --partition=himem2
+#SBATCH --partition=himem
 #SBATCH --qos=himem
 #SBATCH --mail-type=ALL
-#SBATCH --mem=900G
+#SBATCH --mem=300G
 #SBATCH --mail-user=noah.reid@uconn.edu
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
@@ -15,15 +15,15 @@ hostname
 date
 
 # load software
-module load flye/2.8.2
+module load flye/2.8.3
 
-FASTA=../../results/fastas/male_wfails.fasta
+FASTA=../../results/fastas/male.fasta
 
-OUTDIR=../../results/flye_male_wfails
+OUTDIR=../../results/flye_male
 
 
 # run flye
 	# consider "--keep-haplotypes" option
 	# check to see if hybrid ancestry haplotigs are output
 
-flye --nano-raw $FASTA --out-dir $OUTDIR --threads 24
+flye --nano-raw $FASTA --out-dir $OUTDIR --threads 24 --iterations 3
